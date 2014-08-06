85874c8b40bea098a3223eca49378dfe5264750f
Lars Kellogg-Stedman <lars@redhat.com>
2014-08-06 13:28:52 -0400

    initial commit

---
+++ demo_nova_hooks/simple.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+
+class SimpleHookCreate (object):
+
+    logfile = '/var/log/nova/hook-create.log'
+
+    def pre(self, *args, **kwargs):
+        with open(self.logfile, 'a') as fd:
+            print >>fd, 'BEGIN PRE'
+            for i, arg in enumerate(args):
+                print >>fd, '%d: %s' % (i, arg)
+            for k, v in kwargs.items():
+                print >>fd, '%s=%s' % (k, v)
+            print >>fd, 'END PRE'
+
+    def post(self, *args, **kwargs):
+        with open(self.logfile, 'a') as fd:
+            print >>fd, 'BEGIN POST'
+            for i, arg in enumerate(args):
+                print >>fd, '%d: %s' % (i, arg)
+            for k, v in kwargs.items():
+                print >>fd, '%s=%s' % (k, v)
+            print >>fd, 'END POST'
+
+
+class SimpleHookDelete (object):
+
+    logfile = '/var/log/nova/hook-delete.log'
+
+    def pre(self, *args, **kwargs):
+        with open(self.logfile, 'a') as fd:
+            print >>fd, 'BEGIN PRE'
+            for i, arg in enumerate(args):
+                print >>fd, '%d: %s' % (i, arg)
+            for k, v in kwargs.items():
+                print >>fd, '%s=%s' % (k, v)
+            print >>fd, 'END PRE'
+
+    def post(self, *args, **kwargs):
+        with open(self.logfile, 'a') as fd:
+            print >>fd, 'BEGIN POST'
+            for i, arg in enumerate(args):
+                print >>fd, '%d: %s' % (i, arg)
+            for k, v in kwargs.items():
+                print >>fd, '%s=%s' % (k, v)
+            print >>fd, 'END POST'
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name="demo_nova_hooks",
+    version=5,
+    packages=['demo_nova_hooks'],
+    entry_points={
+        'nova.hooks': [
+            'create_instance=demo_nova_hooks.simple.SimpleHookCreate',
+            'delete_instance=demo_nova_hooks.simple.SimpleHookDelete',
+        ]
+    },
+)
