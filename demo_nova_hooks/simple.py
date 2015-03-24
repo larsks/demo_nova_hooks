4fbeee560d9536f31bbaafc6cbbb3685b1ac7e31
Michael Solberg <msolberg@redhat.com>
2015-03-24 16:38:01 -0400

    Add a hook for instance_network_info. See https://blueprints.launchpad.net/nova/+spec/instance-network-info-hook.

--- demo_nova_hooks/simple.py
+++ demo_nova_hooks/simple.py
@@ -21,6 +21,28 @@ class SimpleHookCreate (object):
                 print >>fd, '%d: %s' % (i, arg)
             for k, v in kwargs.items():
                 print >>fd, '%s=%s' % (k, v)
+
+
+class SimpleHookNetwork (object):
+
+    logfile = '/var/log/nova/hook-network.log'
+
+    def pre(self, *args, **kwargs):
+        with open(self.logfile, 'a') as fd:
+            print >>fd, 'BEGIN PRE'
+            for i, arg in enumerate(args):
+                print >>fd, '%d: %s' % (i, arg)
+            for k, v in kwargs.items():
+                print >>fd, '%s=%s' % (k, v)
+            print >>fd, 'END PRE'
+
+    def post(self, *args, **kwargs):
+        with open(self.logfile, 'a') as fd:
+            print >>fd, 'BEGIN POST'
+            for i, arg in enumerate(args):
+                print >>fd, '%d: %s' % (i, arg)
+            for k, v in kwargs.items():
+                print >>fd, '%s=%s' % (k, v)
             print >>fd, 'END POST'
 
 
